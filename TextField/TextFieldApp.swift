93222384315d9c5adb600c1a3ceac193d3feb1c4
Volodymyr <zmiyov88@gmail.com>
2024-08-09 22:08:48 +0300

    Initial Commit

---
+++ TextField/TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldApp.swift
+//  TextField
+//
+//  Created by Volodymyr Pysarenko on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
